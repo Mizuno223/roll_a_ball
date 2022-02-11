b40da9c0b97e794ba12af30c27c3a479f759b73a
Yuto Mizuno <31834@toyota.kosen-ac.jp>
2022-02-11 23:39:55 +0900

    Add function of best time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
     public float speed;
     public float time;
+    public float bestTime;
     public Text countText;
     public Text winText;
     public Text TimeText;
+    public Text BestTimeText;
     private Rigidbody rb;
     private int count;
     public int pickUp;
+    private bool isGameClear = false;
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
@@ -21,10 +25,26 @@ public class PlayerController : MonoBehaviour {
     }
     void Update()
     {
+        bestTime = PlayerPrefs.GetFloat("TIMER", 100);
         if (count < pickUp)
         {
             time += Time.deltaTime;
             TimeText.text = "Time : " + time.ToString("F2");
+        }
+
+        if(bestTime < 100)
+        {
+            BestTimeText.text = "Best time " + bestTime.ToString("F2");
+        }
+        else
+        {
+            BestTimeText.text = "Best time ";
+        }
+
+        if (isGameClear && Input.GetButtonDown("Submit"))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MiniGame");
         }
     }
     void FixedUpdate ()
@@ -33,12 +53,6 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis ("Vertical");
 
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        if (count >= pickUp)
-        {
-            movement = new Vector3 (0.0f, 0.0f, 0.0f);
-        }
-
         rb.AddForce (movement * speed);
     }
 
@@ -57,7 +71,15 @@ public class PlayerController : MonoBehaviour {
         countText.text = "Count : " + count.ToString();
         if (count >= pickUp)
         {
+            isGameClear = true;
             winText.text = "You Win!";
+            Time.timeScale = 0f;
+            if(bestTime > time )
+            {
+                bestTime = time;
+                PlayerPrefs.SetFloat("TIMER", bestTime);
+                PlayerPrefs.Save();
+            }
         }
     }
 }
